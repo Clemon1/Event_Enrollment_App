695d355c89655c4f0bf1d854e78e669652f0c722
CLEMON EZEH <clemonezeh@gmail.com>
2023-02-25 14:31:59 +0100

    finished working on the authSLice

---
+++ frontend/src/features/authSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const user = localStorage.getItem("User");
+
+const initialState = {
+  isLoading: false,
+  User: user ? JSON.parse(user) : null,
+  isError: false,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    Pending: (state) => {
+      state.isLoading = true;
+      state.isError = false;
+    },
+    Success: (state, action) => {
+      state.isLoading = false;
+      state.User = action.payload;
+      localStorage.setItem("User", JSON.stringify(state.User));
+      state.isError = false;
+    },
+    Rejected: (state) => {
+      state.isLoading = false;
+      state.isError = true;
+    },
+    LogOut: (state) => {
+      state.isLoading = false;
+      state.User = null;
+      state.isError = false;
+      localStorage.removeItem("User");
+    },
+  },
+});
+
+export const { Pending, Success, Rejected, LogOut } = authSlice.actions;
+
+export default authSlice.reducer;
