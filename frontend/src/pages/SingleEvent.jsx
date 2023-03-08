1913609bed0ec8f7668f925bce473b333eecfa2d
CLEMON EZEH <clemonezeh@gmail.com>
2023-03-08 23:46:30 +0100

    Set up single booking button

--- frontend/src/pages/SingleEvent.jsx
+++ frontend/src/pages/SingleEvent.jsx
@@ -1,8 +1,18 @@
-import { Box, Stack, Typography, Avatar, Card, CardMedia } from "@mui/material";
+import {
+  Box,
+  Stack,
+  Typography,
+  Avatar,
+  Card,
+  CardMedia,
+  Button,
+} from "@mui/material";
 import { useGetSingleEventsQuery } from "../features/eventSlice";
 import { useParams } from "react-router-dom";
 import LeftSideBar from "../components/leftSidebar";
 import RightSideBar from "../components/rightSidebar";
+import moment from "moment";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 const SingleEvent = () => {
   const { id } = useParams();
   const { data: category } = useGetSingleEventsQuery(id);
@@ -30,17 +40,44 @@ const SingleEvent = () => {
             paddingX: "15px",
             paddingY: 2,
           }}>
-          <Typography variant='h4' color={"#ffffff"}>
-            {" "}
+          <Typography variant='h5' fontWeight={500} color={"#ffffff"}>
             {category && category.title}
           </Typography>
-          <Card sx={{ width: "100%", height: "40vh" }}>
+          <Card sx={{ width: "100%", height: "45vh" }}>
             <CardMedia
               sx={{ width: "100%", height: "100%" }}
               image={category && category.image}
             />
           </Card>
-          <Typography variant='h5' color={"#ffffff"}>
+          <Stack
+            direction={"row"}
+            width={"100%"}
+            height={"4vh"}
+            justifyContent={"space-between"}
+            alignItems={"center"}>
+            <Stack direction={"row"} gap={1}>
+              <CalendarMonthIcon sx={{ color: "#42f1c1" }} />
+              <Typography variant='body1' color={"#42f1c1"} fontWeight={500}>
+                {category &&
+                  moment(category.dateOfEvent).format(" MMMM Do YYYY")}
+              </Typography>
+            </Stack>
+
+            <Button
+              sx={{
+                bgcolor: "#42f1c1",
+                color: "primary.main",
+                borderRadius: 3,
+                fontWeight: 600,
+                ":hover": {
+                  bgcolor: "#42f1c1",
+                },
+              }}>
+              {" "}
+              Book Event
+            </Button>
+          </Stack>
+          <Typography variant='h6' fontWeight={400} color={"#ffffff"}>
             {category && category.description}
           </Typography>
         </Stack>
